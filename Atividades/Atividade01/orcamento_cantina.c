ac252152a8b8ded0f9030b2a24af85c403f22697
Caiouilherme427 <caio.guilherme@iesb.edu.br>
2026-09-09 17:57:11 -0300

    Atualização Atividade01 realizada no terminal do VS Code

--- Atividades/Atividade01/orcamento_cantina.c
+++ Atividades/Atividade01/orcamento_cantina.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 
-#define PRECO_REFEICAO 12.50
+#define PRECO_TICKET 12.50
 #define PRECO_CAFE 4.00
 
 int main() {
-    int qtd_refeicoes, qtd_cafes;
-    float valor_disponivel;
-    float gasto_refeicoes, gasto_cafes, gasto_total, saldo_restante;
+    int qtd_ref, qtd_cafes;
+    float disponivel_cartao;
+    float gasto_ref, gasto_cafes, gasto_total, saldo_restante;
 
     printf("Quantidade de refeicoes: ");
-    scanf("%d", & qtd_refeicoes);
+    scanf("%d", & qtd_ref);
 
     printf("Quantidade de cafes: ");
     scanf("%d", & qtd_cafes);
 
     printf("Valor disponivel no cartao: ");
-    scanf("%f", & valor_disponivel);
+    scanf("%f", & disponivel_cartao);
 
-    gasto_refeicoes = qtd_refeicoes * PRECO_REFEICAO;
+    gasto_ref = qtd_ref * PRECO_TICKET;
     gasto_cafes = qtd_cafes * PRECO_CAFE;
-    gasto_total = gasto_refeicoes + gasto_cafes;
-    saldo_restante = valor_disponivel - gasto_total;
+    gasto_total = gasto_ref + gasto_cafes;
+    saldo_restante = disponivel_cartao - gasto_total;
 
-    printf("\nGasto refeicoes: %.2f\n", gasto_refeicoes);
+    printf("\nGasto refeicoes: %.2f\n", gasto_ref);
     printf("Gasto cafes: %.2f\n", gasto_cafes);
     printf("Gasto total: %.2f\n", gasto_total);
     printf("Saldo restante: %.2f\n", saldo_restante);
